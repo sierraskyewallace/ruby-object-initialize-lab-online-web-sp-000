e1a3efa0b905a4aab124eecffbd67fe8d03dbf57
Sierra Wallace <sskwallace@gmail.com>
2020-08-28 02:19:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,13 @@
 class Dog 
+    def initialize(name)
+      @name = name 
+    end
+    def name=(name)
+      @name = name 
+    end
+    def name
+      @name
+    end
     def initialize(breed)
     @breed = breed
   end
